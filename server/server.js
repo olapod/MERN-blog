8f80f1f3b60fcea8eece22d4b4b6c63f77b3206b
Ola Podsiadlik <alecs@op.pl>
2019-07-19 22:29:06 +0200

    Change server configuration

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const config = require('./config');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// import routes
+const postRoutes = require('./routes/post.routes');
+
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use('/api', postRoutes);
 
-app.get('/api/posts', (req, res) => {
-  const data = [
-    { id: '1adfasf', title: 'Lorem Ipsum', content: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.' },
-    { id: '2evxc34', title: 'Lorem Ipsum II', content: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.' },
-  ]
-  res.json(data);
-});
+// connects our back end code with the database
+mongoose.connect(config.DB, { useNewUrlParser: true });
+let db = mongoose.connection;
 
-app.listen(8000, function(){
-  console.log('Server is running on port:', 8000);
+db.once('open', () => console.log('Connected to the database'));
+db.on('error', (err) => console.log('Error ' + err));
+
+app.listen(config.PORT, function() {
+  console.log('Server is running on Port:', config.PORT);
 });
