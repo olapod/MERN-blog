df747e5a66b01fbd025b7b730a19eadadc15f953
Ola Podsiadlik <alecs@op.pl>
2019-10-01 21:33:44 +0200

    Add home and contact pages

--- client/src/components/pages/Contact/ContactPage.js
+++ client/src/components/pages/Contact/ContactPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PageTitle from '../../common/PageTitle/PageTitle';
 
 const ContactPage = () => (
   <div>
-    <h1>Contact</h1>
-  </div>
+    <PageTitle>Contact</PageTitle>
+    <p>Your Favourite Blooger Ltd.</p>
+    <p>9432 Division Street</p>
+    <p>Stone Mountain, GA 30083</p><br/>
+    <p>Mobile Number: 207-914-3411</p>
+</div>
 );
 
 export default ContactPage;
--- client/src/components/pages/Home/HomePage.js
+++ client/src/components/pages/Home/HomePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import PageTitle from '../../common/PageTitle/PageTitle';
+import Posts from '../../features/Posts/PostsContainer';
 
 const HomePage = () => (
   <div>
-    <h1>Home</h1>
-  </div>
+  <PageTitle>Blog</PageTitle>
+  <Posts postsPerPage={1} pagination={false} />
+</div>
 );
 
 export default HomePage;
